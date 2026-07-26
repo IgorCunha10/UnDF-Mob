1d12ae9021b67e84d168e4bf75b96211b626267c
Igor Cunha <gigor497@gmail.com>
2026-07-26 12:50:57 -0300

    Initial configurations && packages

---
+++ app/src/main/java/com/example/mob/presentation/screens/login/LoginScreen.kt
@@ -0,0 +1,12 @@
+package com.example.mob.presentation.screens.login
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun LoginScreen() {
+
+}
