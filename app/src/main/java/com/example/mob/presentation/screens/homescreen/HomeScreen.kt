4754f14caed121716b873e23bcf18154fc3f6f3a
Igor <gigor497@gmail.com>
2026-07-29 15:57:49 -0300

    TODO: Implement Carousel

--- app/src/main/java/com/example/mob/presentation/screens/homescreen/HomeScreen.kt
+++ app/src/main/java/com/example/mob/presentation/screens/homescreen/HomeScreen.kt
@@ -12,6 +12,7 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -24,6 +25,7 @@ import com.example.mob.R
 
 @Composable
 fun HomeScreen() {
+
 
     Box(modifier = Modifier.fillMaxSize()
         .background(Color(0xFFFFFF))) {
