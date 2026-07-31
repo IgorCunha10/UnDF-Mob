9a111552b33838871261356da28d0d3583a955ee
Igor Cunha <gigor497@gmail.com>
2026-07-30 23:08:51 -0300

    Implemented detach carousel.

--- app/src/main/java/com/example/mob/presentation/components/Carousel.kt
+++ app/src/main/java/com/example/mob/presentation/components/Carousel.kt
@@ -4,6 +4,7 @@ import android.R.attr.contentDescription
 import androidx.annotation.DrawableRes
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -11,6 +12,7 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.material3.carousel.HorizontalMultiBrowseCarousel
 import androidx.compose.material3.carousel.rememberCarouselState
 import androidx.compose.runtime.Composable
@@ -23,7 +25,7 @@ import com.example.mob.R
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Carousel() {
+fun TopCarousel() {
     data class CarouselItem(
         val id: Int,
         @DrawableRes val imageResId: Int,
@@ -32,13 +34,15 @@ fun Carousel() {
 
     val items = remember {
         listOf(
-            CarouselItem(0, R.drawable.bannertwo,
-                "Art-Banner"),
-            CarouselItem(1, R.drawable.bannerthree,
-                "Linguistcs-Banner"),
-            CarouselItem(2, R.drawable.bannerone,
+            CarouselItem(0, R.drawable.bannerone,
                 "Banner-One"),
-            CarouselItem(3, R.drawable.bannerfive, "Banner-Two")
+            CarouselItem(1, R.drawable.bannerfive,
+                "Banner-Two"),
+            CarouselItem(2, R.drawable.bannerthree,
+                "Linguistics-Banner"),
+            CarouselItem(3, R.drawable.bannertwo,
+                "Art-Banner")
+
         )
     }
 
@@ -46,7 +50,7 @@ fun Carousel() {
         state = rememberCarouselState{ items.count() },
         modifier = Modifier.fillMaxWidth()
             .wrapContentHeight()
-            .padding(top = 48.dp, bottom = 16.dp),
+            .padding(top = 50.dp, bottom = 16.dp),
         preferredItemWidth = 400.dp,
         itemSpacing = 8.dp,
         contentPadding = PaddingValues(horizontal = 16.dp)
@@ -56,11 +60,20 @@ fun Carousel() {
 
         Image(
             modifier = Modifier
-                .height(205.dp)
+                .height(210.dp)
                 .maskClip(MaterialTheme.shapes.extraLarge),
             painter = painterResource(id = item.imageResId),
             contentDescription = item.contentDescription,
             contentScale = ContentScale.Crop
         )
     }
+}
+
+@Composable
+fun MidCarousel() {
+
+    val items = remember {
+
+    }
+
 }
--- app/src/main/java/com/example/mob/presentation/screens/homescreen/HomeScreen.kt
+++ app/src/main/java/com/example/mob/presentation/screens/homescreen/HomeScreen.kt
@@ -1,23 +1,61 @@
 package com.example.mob.presentation.screens.homescreen
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.absolutePadding
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
-import com.example.mob.presentation.components.Carousel
+import androidx.compose.ui.unit.sp
+import com.example.mob.R
+import com.example.mob.presentation.components.TopCarousel
 
 @Composable
 fun HomeScreen() {
 
-    Box(modifier = Modifier.fillMaxWidth()
-        .padding(10.dp)) {
+    Column(modifier = Modifier.fillMaxWidth()
+        .absolutePadding(top = 40.dp, left = 25.dp, right = 20.dp)) {
 
+        Row(modifier = Modifier.fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Start) {
 
+            Image(
+                painter = painterResource(R.drawable.profile),
+                contentDescription = "Profile Icon",
+                modifier = Modifier.size(24.dp)
+            )
+
+            Spacer(modifier = Modifier.absolutePadding(right = 10.dp))
+
+            Text(text = "Bem vindo, Usuário",
+                fontSize = 18.sp,
+                fontWeight = FontWeight.Bold)
+
+            Spacer(modifier = Modifier.absolutePadding(right = 150.dp))
+
+            Image(
+                painter = painterResource(R.drawable.notification),
+                contentDescription = "Notification Icon",
+                modifier = Modifier.size(32.dp)
+            )
+        }
+
+        TopCarousel()
 
     }
 
+//    Spacer(modifier = Modifier.padding(40.dp))
 
-    Carousel()
 }
